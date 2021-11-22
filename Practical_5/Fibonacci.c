d1aaa0286b1c49fe0dcb185f28a13770e30847e7
RashmiPandit1 <92164111+RashmiPandit1@users.noreply.github.com>
2021-11-22 22:32:35 +0000

    Made some changes to the code and added more comments

--- Practical_5/Fibonacci.c
+++ Practical_5/Fibonacci.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+//Function with arguments Fn−1 and Fn−2 and on exit they will have Fn and Fn−1.
 void tranform(int *f1, int *f2)
 {
     int fn = *f1 + *f2;
     *f1 = *f2;
     *f2 = fn;
 }
+//Function to print the values of the Fibonacci series
 void solve(int n)
 {
     int f1 = 0, f2 = 1;
@@ -13,6 +15,7 @@ void solve(int n)
     if(n >= 2)
         printf("%d ", f2);
     n -= 2;
+    //loop to find the whole series upto n
     while(n > 0)
     {
         n--;
@@ -20,6 +23,7 @@ void solve(int n)
         printf("%d ", f2);
     }
 }
+//Main handler
 int main()
 {
     int n;
